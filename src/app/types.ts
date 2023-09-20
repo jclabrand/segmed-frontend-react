5235fc61297aa0df10517fadcfd708bdd2c0b49e
Juan Carlos <jcharlylc@gmail.com>
2023-09-20 14:53:13 -0400

    Basic user type

---
+++ src/app/types.ts
@@ -0,0 +1,13 @@
+
+/*	Copyright (C) 2023, All Rights Reserved
+ *	Written by Juan Carlos Labrandero <jcharly@labrandero.com>
+ */
+
+
+export type TUser = {
+	userName:		string
+	displayName?:	string
+	email?:			string
+
+	isAuthorized:	boolean
+}
