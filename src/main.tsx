bcde6929753fcf004859be2a010ed4b5cf971291
Juan Carlos <jcharlylc@gmail.com>
2023-09-22 11:43:41 -0400

    Authorization integration

--- src/main.tsx
+++ src/main.tsx
@@ -9,16 +9,36 @@ import ReactDOM from 'react-dom/client'
 import { setContext } from '@apollo/client/link/context'
 import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache, concat } from '@apollo/client'
 
+import { getAuth } from './app/client'
 import { App } from './app/app'
+
 
 function main() {
 	const authLink = setContext(() => {
-		return {}
+		const { bearer, token } = getAuth()
+
+		return {
+			headers: {
+				authorization: `${bearer} ${ token }`,
+			}
+		}
 	})
 
 	const client = new ApolloClient({
 		link: concat(authLink, new HttpLink({ uri: 'http://localhost:3000/graphql' })),
-		cache: new InMemoryCache({})
+		cache: new InMemoryCache({
+			typePolicies: {
+				User: {
+					fields: {
+						isAuthorized: {
+							read() {
+								return true
+							}
+						}
+					}
+				}
+			}
+		})
 	})
 
 	return {
---
+++ src/utils/index.ts
@@ -0,0 +1,12 @@
+
+/*	Copyright (C) 2023, All Rights Reserved
+ *	Written by Juan Carlos Labrandero <jcharly@labrandero.com>
+ */
+
+
+import Error from './error'
+
+
+export {
+	Error
+}
