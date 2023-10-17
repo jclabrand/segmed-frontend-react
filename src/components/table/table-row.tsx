c4ab5c6e3ec0f7364523af1471414769b5744184
Juan Carlos <jcharlylc@gmail.com>
2023-10-16 23:27:15 -0400

    Table component

---
+++ src/components/table/table-row.tsx
@@ -0,0 +1,60 @@
+
+/*	Copyright (C) 2023, All Rights Reserved
+ *	Written by Juan Carlos Labrandero <jcharly@labrandero.com>
+ */
+
+
+import { ReactElement, useEffect, useRef, useState } from 'react'
+
+
+export interface TableCellCallback<TData> {
+	(row: TData, index: number): ReactElement | string | number | boolean
+}
+
+type TableRowProps<TData> = {
+	rows:	TableCellCallback<TData>[]
+	item:	TData
+}
+
+function useTableRow() {
+	const [ visible, setVisible ] = useState(false)
+
+	const observe: IntersectionObserverCallback = (entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				setVisible(true)
+				observer.disconnect()
+			}
+		})
+	}
+
+	const lazy = useRef<HTMLTableSectionElement>(null)
+		, io = useRef(new IntersectionObserver(observe))
+
+	useEffect(() => {
+		if (lazy.current)
+			io.current.observe(lazy.current)
+		
+		return () => {}
+	}, [])
+
+	return { lazy, visible }
+}
+
+export function TableRow<TData>(props: TableRowProps<TData>) {
+	const { lazy, visible } = useTableRow()
+
+	return (
+		<tbody ref={ lazy }>
+			<tr>
+				{
+					visible
+						? props.rows.map((cell, j) => {
+							return (<td key={j}>{ cell(props.item, j) }</td>)
+						})
+						: <td></td>
+				}
+			</tr>
+		</tbody>
+	)
+}
---
+++ src/components/table/table.tsx
@@ -0,0 +1,43 @@
+
+/*	Copyright (C) 2023, All Rights Reserved
+ *	Written by Juan Carlos Labrandero <jcharly@labrandero.com>
+ */
+
+
+import { ReactNode } from 'react'
+
+import { TableRow, TableCellCallback } from './table-row'
+import './table.css'
+
+
+type TableProps<TData> = {
+	children?:	ReactNode
+	columns:	string[]
+	rows:		TableCellCallback<TData>[]
+	dataset:	TData[]
+}
+
+function Table<TData>(props: TableProps<TData>) {
+	
+	return (
+		<div className='table'>
+			<table>
+				<thead>
+					<tr>
+						{ props.columns.map((col, i) => (<th key={i}>{ col }</th>)) }
+					</tr>
+				</thead>
+				
+				{ props.dataset.map((item, i) => (<TableRow<TData> key={i} item={item} rows={props.rows}/>)) }
+
+			</table>
+		</div>
+	)
+}
+
+Table.defaultProps = {
+	rows: [],
+	dataset: []
+}
+
+export default Table
